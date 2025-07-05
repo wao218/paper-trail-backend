3fa0c8a987d9bafa8d46ac7a247f209e8d70d923
wao218 <wesleyosborne1@gmail.com>
2025-07-05 15:09:22 -0400

    feat: Conversation history

--- src/services/langchainService.ts
+++ src/services/langchainService.ts
@@ -7,6 +7,7 @@ import {
   RunnablePassthrough,
   RunnableSequence,
 } from '@langchain/core/runnables';
+import { formatConvHistory } from '../utils/formatConvHistory';
 
 const embeddings = new OllamaEmbeddings({
   model: 'nomic-embed-text',
@@ -23,15 +24,18 @@ const llm = new ChatOllama({
 
 const retriever = vectorStore.asRetriever();
 
-const standaloneQuestionTemplate =
-  'Given a question, convert it to a standalone question. Just give the question by itself. question: {question} standalone question:';
+const standaloneQuestionTemplate = `Given a question and some conversation history (if any), convert the question into a standalone question. Just give the question by itself.
+  question: {question}
+  history: {history}
+  standalone question:`;
 
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
   standaloneQuestionTemplate
 );
 
-const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question based on the PDF documents a user provides as context. Try to find the answer in the context provided. If you really don't know the answer, say in a friendly nice way "I'm sorry, I don't know the answer to that." Don't try to make up an answer. Always speak as if you were chatting with a good friend.
+const answerTemplate = `You are an experience, helpful and enthusiastic researcher and an expert at interpretting and answering questions based on the PDF documents a user provides as context and the conversation history. Try to find the answer in the context provided. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say in a friendly nice way "I'm sorry, I don't know the answer to that." Don't try to make up an answer. Always speak as if you were chatting with a good friend.
 context: {context}
+history: {history}
 question: {question}
 answer:
 `;
@@ -62,12 +66,20 @@ const chain = RunnableSequence.from([
   {
     context: retrieverChain,
     question: ({ original_input }) => original_input.question,
+    history: ({ original_input }) => original_input.history,
   },
   answerChain,
 ]);
 
+const convHistory: string[] = [];
+
 export async function chatWithPDF(question: string) {
-  const response = await chain.invoke({ question });
+  const response = await chain.invoke({
+    question,
+    history: formatConvHistory(convHistory),
+  });
+  convHistory.push(question);
+  convHistory.push(response);
   console.log(response);
   return response;
 }
